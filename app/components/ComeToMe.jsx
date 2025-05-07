162c731c1b65289cc2349cb5cf9f80895cb051b5
Peter <skwortsow.petia@yandex.ru>
2025-05-07 17:52:35 +0300

    Правки

--- app/components/ComeToMe.jsx
+++ app/components/ComeToMe.jsx
@@ -3,33 +3,40 @@ import Image from 'next/image';
 export default function ComeToMe() {
   return (
     <>
-      <div className="hero pl-8 pr-10 mb-20">
+      <div className="hero mb-20">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="easel.jpeg" className="max-w-sm rounded-lg shadow-2xl" />
+          <Image
+            src="/easel.jpeg"
+            width={200}
+            height={100}
+            alt="Picture of the author"
+            style={{ width: "100%" }}
+            className=" rounded-lg shadow-2xl max-w-lg"
+          />
           <div>
             <h6 className="text-2xl font-bold pt-4">Приходите ко мне если:</h6>
-            <ul className="list-disc p-6">
-              <li className="p-1">
+            <ul className="list-disc pr-5 pl-5">
+              <li className="mt-5 pb-1">
                 На занятиях вы узнаете о различных техниках и стилях рисования,
                 о свойствах материалов и о том, как правильно их использовать.
                 Это расширит ваш кругозор и поможет лучше понимать искусство.
               </li>
-              <li className="p-1">
+              <li className="pb-1">
                 Есть желание пообщаться с единомышленниками, поделиться мыслями
                 и идеями, рассказать о своих страхах и переживаниях в творческой
                 направленности.
               </li>
-              <li className="p-1">
+              <li className="pb-1">
                 Дети со мной развивают не только навыки рисования, но и
                 уверенность в себе, учатся работать в команде и выражать свои
                 эмоции через творчество.
               </li>
-              <li className="p-1">
+              <li className="pb-1">
                 Практика и совершенствование навыков. Чем больше вы будете
                 практиковаться, тем увереннее будете чувствовать себя в
                 творчестве.
               </li>
-              <li className="p-1">
+              <li className="pb-1">
                 Студия — место для выражения мыслей и чувств через творчество,
                 что помогает снять стресс.
               </li>
